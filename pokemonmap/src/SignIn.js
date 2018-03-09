67a4701a65439a1a7ec6e4678f7423bfaee44700
kkudumu <kkudumu@gmail.com>
2018-03-09 04:45:30 -0800

    Pokemon App Project Complete

---
+++ pokemonmap/src/SignIn.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {View, Text, ImageBackground, Image, Dimensions} from 'react-native';
+import {Form, Item, Label, Input, Button} from 'native-base';
+
+
+var myBackground = require('../assets/landing.jpg');
+var height = Dimensions.get('window').height;
+var width = Dimensions.get('window').width;
+
+class SignIn extends React.Component{
+    state = {
+        email: "",
+        password: ""
+    }
+
+    logIn = () => {
+        var email = this.state.email;
+        var password = this.state.password;
+
+        this.props.signIn(email, password);
+
+    }
+
+    render(){
+        return(
+            <View style={{flex: 1}}>
+            <ImageBackground source={myBackground} style={styles.backgroundImage}>
+                <View style={styles.inputStyle}>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>Email</Label>
+                            <Input 
+                                autoCorrect={false}
+                                onChangeText={(email) =>this.setState({email})}
+                            />
+                        </Item>
+                        <Item floatingLabel>
+                            <Label>Password</Label>
+                            <Input 
+                                autoCorrect={false}
+                                onChangeText={(password) =>this.setState({password})}
+                                secureTextEntry
+                            />
+                        </Item>
+                    </Form>
+                    <View style={{marginTop: 10}}>
+                        <Button
+                            primary
+                            block
+                            onPress={this.logIn}
+                        >
+                            <Text style={{color: 'white'}}>Sign In/Sign Up</Text>
+                        </Button>
+                    
+                    </View>
+                </View>
+            </ImageBackground>
+            </View>
+        )
+    }
+}
+
+const styles = {
+    backgroundImage: {
+        flex: 1,
+        width: width,
+        height: height
+    },
+    inputStyle: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        margin: 10
+    }
+}
+
+export default SignIn;
---
+++ pokeserver/imports/collections/pokemon.js
@@ -0,0 +1,9 @@
+import {Mongo} from 'meteor/mongo';
+import {Meteor} from 'meteor/meteor';
+
+export const Pokemon = new Mongo.Collection('pokemon');
+
+Meteor.publish('pokemon', ()=>{
+    return Pokemon.find({});
+
+});
